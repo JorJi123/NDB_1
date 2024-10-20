648505ab59603e082603bb3f455166b5cfa0b7fb
George <gerych1000@gmail.com>
2024-10-20 19:37:29 +0300

    initial front for eshop

--- frontend/src/eshop/eshop.component.ts
+++ frontend/src/eshop/eshop.component.ts
@@ -6,6 +6,13 @@ import { InputTextModule } from 'primeng/inputtext';
 import { Client } from './client.model';
 import { Product } from './product.model';
 import { EshopService } from './eshop.service';
+import { ListboxModule } from 'primeng/listbox';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { Order } from './order.model';
+import { InputNumberModule } from 'primeng/inputnumber';
+
+
 
 @Component({
   selector: 'app-eshop',
@@ -14,26 +21,73 @@ import { EshopService } from './eshop.service';
             TableModule,
             InputTextModule, 
             FormsModule,
+            ListboxModule,
+            AsyncPipe,
+            InputNumberModule
   ],
   templateUrl: './eshop.component.html',
   styleUrl: './eshop.component.css'
 })
 export class EshopComponent {
+  availableProducts$:Observable<Product[]>;
+  selectedProducts:Product[] = [];
 
-  constructor(private eshopService:EshopService){}
+  constructor(private eshopService:EshopService){
+    this.availableProducts$ = eshopService.getAllProducts();
+  }
+
+  
 
   Submit(){
-    this.eshopService.getAllProducts().subscribe(console.log);
+    //this.eshopService.getAllProducts().subscribe(console.log);
+    console.log(this.formData3);
   };
   formData:Client = {
-    id: '',
-    name: '',
-    email: ''
+    id: "",
+    name: "",
+    email: ""
   };
   formData2:Product = {
     id: '',
     name: '',
-    description: '',
+    category:'',
     price: 0.0
   };
+  formData3:Order = {
+    clientId:'',
+    items:[]
+  }
+
+  SubmitClient(){
+    this.eshopService.registerClient(this.formData).subscribe(console.log);
+  }
+  SubmitProduct(){
+    this.eshopService.registerProduct(this.formData2).subscribe();
+  }
+  SubmitOrder(){
+    this.eshopService.registerOrder(this.formData3).subscribe();
+  }
+  updateDataItems(){
+    const productList = this.selectedProducts.map((product) =>{
+      const existingItem = this.formData3.items.find(item => item.productId === product.id)
+      if(existingItem) return {productId:product.id, quantity: existingItem.quantity }
+      else return {productId:product.id, quantity: 1};
+    })
+    this.formData3.items = productList;
+    console.log(this.formData3);
+  }
+
+  getProductQuantity(id:string){
+  }
+
+  setProductQuantity(id:string, event:any){
+    const existingItem = this.formData3.items.find(item => item.productId === id);
+    
+    if (existingItem) {
+      existingItem.quantity = event;
+    }
+    console.log(this.formData3);
+  }
+
+
 }
--- frontend/src/eshop/eshop.service.ts
+++ frontend/src/eshop/eshop.service.ts
@@ -1,5 +1,9 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Inject, Injectable } from "@angular/core";
+import { from, Observable } from "rxjs";
+import { Product } from "./product.model";
+import { Client } from "./client.model";
+import { Order } from "./order.model";
 
 @Injectable({
     providedIn:'root'
@@ -7,7 +11,20 @@ import { Inject, Injectable } from "@angular/core";
 export class EshopService{
     constructor(private http:HttpClient){}
 
-    getAllProducts(){
-        return this.http.get('products');
+    getAllProducts():Observable<Product[]>{
+        return this.http.get<Product[]>('products');
+    }
+
+    registerClient(form:Client){
+        const headers = new HttpHeaders('Content-Type: application/json')
+        return this.http.put('clients',form, {headers: headers});
+    }
+
+    registerOrder(form:Order){
+        return this.http.put('orders', form);
+    }
+
+    registerProduct(form:Product){
+        return this.http.put('products', form);
     }
 }
